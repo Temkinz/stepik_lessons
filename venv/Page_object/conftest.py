a37026b4fd7c46d86ef7623aa8af9beb8af1b100
Temkinz <50596199+Temkinz@users.noreply.github.com>
2020-09-27 17:49:41 +0300

    initial commit

---
+++ venv/Page_object/conftest.py
@@ -0,0 +1,29 @@
+
+import pytest
+from selenium import webdriver
+import time
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose language")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    print("\nstart browser for test..")
+    language = request.config.getoption("language")
+    languages_list = ["de", "ar", "ca", "cs", "da", "en-gb", "el", "es", "fi", "fr", "it", "ko", "nl", "pl", "pt", "pt-br", "ro", "ru", "sk", "uk", "zh-hans"]
+    if language is None:
+        language == "en"
+    elif language not in languages_list:
+        raise pytest.UsageError("--language should be chosen from the existing list")
+
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    browser = webdriver.Chrome(options=options)
+    browser.implicitly_wait(5)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
